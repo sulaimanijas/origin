191067998e160df6d88921f0015ac6de111cda28
sulaimanijas <syed_sulaiman@dlighttech.in>
2022-04-17 23:06:15 -0700

    3rd commit

--- sample/sample/ViewController.swift
+++ sample/sample/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     var name =  "sulaiman"
     var name2 =  "syed"
+    var name3 = "ejas"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
