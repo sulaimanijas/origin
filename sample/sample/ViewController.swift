572068fba8c86c0d35edc1a8ccbe1bb38f8ff453
sulaimanijas <syed_sulaiman@dlighttech.in>
2022-04-17 23:02:01 -0700

    1st commit

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Sulaiman on 17/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var name =  "sulaiman"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
