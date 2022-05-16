c1b73fd47b8826eab940a5d68ad1f4b13d2e469c
sulaimanijas <syed_sulaiman@dlighttech.in>
2022-05-16 15:15:42 +0530

    loaded txt in to html

--- sample/sample/ViewController.swift
+++ sample/sample/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var name =  "sulaiman"
+    @IBOutlet weak  var lblContent : UILabel!
+    @IBOutlet weak var webView : UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        func getdata(){
-            let name = "nil"
+            do {
+                guard let filePath = Bundle.main.path(forResource: "sample_email", ofType: "txt")
+                    else {
+                        // File Error
+                        print ("File reading error")
+                        return
+                }
+
+                let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
+                let baseUrl = URL(fileURLWithPath: filePath)
+                webView.loadHTMLString(contents as String, baseURL: baseUrl)
+            }
+            catch {
+                print ("File HTML error")
+            }
+        }
         }
         
-    }}
+    
