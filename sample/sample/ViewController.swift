a1a39c5e26573115394d534602de7cb0c584e5db
sulaimanijas <syed_sulaiman@dlighttech.in>
2022-05-16 15:17:13 +0530

    ordered

--- sample/sample/ViewController.swift
+++ sample/sample/ViewController.swift
@@ -12,22 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var webView : UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-            do {
-                guard let filePath = Bundle.main.path(forResource: "sample_email", ofType: "txt")
-                    else {
-                        // File Error
-                        print ("File reading error")
-                        return
-                }
-
-                let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
-                let baseUrl = URL(fileURLWithPath: filePath)
-                webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        do {
+            guard let filePath = Bundle.main.path(forResource: "sample_email", ofType: "txt")
+            else {
+                // File Error
+                print ("File reading error")
+                return
             }
-            catch {
-                print ("File HTML error")
-            }
+            
+            let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: filePath)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
         }
+        catch {
+            print ("File HTML error")
         }
+    }
+}
         
     
