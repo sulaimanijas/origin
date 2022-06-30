8daaecd2d8cc8f58ed9dbaea1c1294f3ec702602
sulaimanijas <syed_sulaiman@dlighttech.in>
2022-06-30 15:32:30 +0530

    array declared

---
+++ test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Sulaiman on 30/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ var array = [":dcbhj"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
